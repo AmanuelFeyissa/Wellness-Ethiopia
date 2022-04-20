7347db161c5f21034d63153791760e4c6cc1a832
Amanuel Feyissa <43337227+AmanuelFeyissa@users.noreply.github.com>
2022-04-20 12:05:10 +0300

    Delete wellness_ethiopia directory
    had alot of missing parts, i will reupload a new repo

--- wellness_ethiopia/README.md
+++
@@ -1,3 +0,0 @@
-# wellness_Ethiopia
-
-A mobile based mental health counseling and community App
--- wellness_ethiopia/android/app/src/main/kotlin/com/example/wellness_ethiopia/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.wellness_ethiopia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
