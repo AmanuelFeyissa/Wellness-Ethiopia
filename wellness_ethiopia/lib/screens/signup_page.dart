f0fe0608e8b455f469b2f0bfba1ef2b34d2a8f02
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-04-21 19:29:46 +0300

    Create signup_page.dart
    done with the sign up page, not completely responsive

---
+++ wellness_ethiopia/lib/screens/signup_page.dart
@@ -0,0 +1,103 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(top: 120.0, bottom: 30.0),
+              height: 100.0,
+              width: 100.0,
+              child: Image.asset('images/mentalPuzzle.png'),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Wellness Ethiopia',
+              style: TextStyle(
+                fontSize: 45.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 70.0,
+            width: 120.0,
+            child: Divider(
+              color: Colors.white38,
+              thickness: 2.0,
+            ),
+          ),
+          Center(
+            child: TextButton(
+              onPressed: () {
+                // TODO Add a route to signup form
+              },
+              child: Container(
+                padding: EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  'Signup as a Professional',
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Center(
+            child: TextButton(
+              onPressed: () {
+                // TODO Add a route to signup form
+              },
+              child: Container(
+                padding: EdgeInsets.fromLTRB(78.0, 20.0, 78.0, 20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  'Signup as a User',
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Text(
+            'Already have an account?',
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.white54,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              // TODO Add a route to the next page
+            },
+            child: Text(
+              'Sign in',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
