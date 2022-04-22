fb6b5020100aed003804e39e8076e39602478c5c
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-04-22 15:25:25 +0300

    Update main.dart
    tested home screen

--- wellness_ethiopia/lib/main.dart
+++ wellness_ethiopia/lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:wellness_ethiopia/screens/get_started_page.dart';
+import 'package:wellness_ethiopia/screens/home_screen.dart';
 import 'package:wellness_ethiopia/screens/register_for_pro_page.dart';
 import 'package:wellness_ethiopia/screens/register_for_user_page.dart';
 import 'package:wellness_ethiopia/screens/signin_page.dart';
