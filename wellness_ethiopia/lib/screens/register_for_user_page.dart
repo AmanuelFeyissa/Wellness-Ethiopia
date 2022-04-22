52748217307ad5df04e8f0084bb2767eca70fc66
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-04-22 14:08:37 +0300

    Update register_for_user_page.dart
    refactored ans made it more object oriented

--- wellness_ethiopia/lib/screens/register_for_user_page.dart
+++ wellness_ethiopia/lib/screens/register_for_user_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:wellness_ethiopia/constants.dart';
+import 'package:wellness_ethiopia/utilities/commonWhiteContainers.dart';
+import 'package:wellness_ethiopia/utilities/formInputWidget.dart';
 
 class RegisterForUserPage extends StatelessWidget {
   const RegisterForUserPage({Key? key}) : super(key: key);
@@ -127,59 +129,6 @@ class RegisterForUserPage extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class FormInputWidget extends StatelessWidget {
-  FormInputWidget({@required this.icon, required this.topMargin});
-
-  final IconData? icon;
-  double topMargin = 30.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(5.0),
-      margin: EdgeInsets.fromLTRB(15.0, topMargin, 10.0, 0.0),
-      height: 50.0,
-      width: 50.0,
-      decoration: BoxDecoration(
-        color: kPreferedGreyColor,
-        borderRadius: BorderRadius.circular(3),
-      ),
-      child: Row(
-        children: [
-          // Icon
-          Icon(
-            icon,
-            color: kMainBackgroundColor,
-            size: 30.0,
-          ),
-          // TextField
-          Text('Hello World'),
-        ],
-      ),
-    );
-  }
-}
-
-class CommonWhiteContainer extends StatelessWidget {
-  CommonWhiteContainer({@required this.insideTheBox});
-
-  final Widget? insideTheBox;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: insideTheBox,
-      margin: EdgeInsets.all(5),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(60),
-        ),
       ),
     );
   }
