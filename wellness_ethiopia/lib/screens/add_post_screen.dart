42c61582ea596dbc3322d0f2e930444db0cbff7a
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-06-03 22:17:55 +0300

    post text feature

--- wellness_ethiopia/lib/screens/add_post_screen.dart
+++ wellness_ethiopia/lib/screens/add_post_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:wellness_ethiopia/providers/user_provider.dart';
 import 'package:wellness_ethiopia/resources/firestore_methods.dart';
+import 'package:wellness_ethiopia/services/posts.dart';
 import 'package:wellness_ethiopia/utilities/colors.dart';
 import 'package:wellness_ethiopia/utilities/utils.dart';
 import 'package:provider/provider.dart';
@@ -19,46 +20,47 @@ class _AddPostScreenState extends State<AddPostScreen> {
   Uint8List? _file;
   bool isLoading = false;
   final TextEditingController _descriptionController = TextEditingController();
+  final PostService _postService = PostService();
 
-  _selectImage(BuildContext parentContext) async {
-    return showDialog(
-      context: parentContext,
-      builder: (BuildContext context) {
-        return SimpleDialog(
-          title: const Text('Create a Post'),
-          children: <Widget>[
-            SimpleDialogOption(
-                padding: const EdgeInsets.all(20),
-                child: const Text('Take a photo'),
-                onPressed: () async {
-                  Navigator.pop(context);
-                  Uint8List file = await pickImage(ImageSource.camera);
-                  setState(() {
-                    _file = file;
-                  });
-                }),
-            SimpleDialogOption(
-                padding: const EdgeInsets.all(20),
-                child: const Text('Choose from Gallery'),
-                onPressed: () async {
-                  Navigator.of(context).pop();
-                  Uint8List file = await pickImage(ImageSource.gallery);
-                  setState(() {
-                    _file = file;
-                  });
-                }),
-            SimpleDialogOption(
-              padding: const EdgeInsets.all(20),
-              child: const Text("Cancel"),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )
-          ],
-        );
-      },
-    );
-  }
+  // _selectImage(BuildContext parentContext) async {
+  //   return showDialog(
+  //     context: parentContext,
+  //     builder: (BuildContext context) {
+  //       return SimpleDialog(
+  //         title: const Text('Create a Post'),
+  //         children: <Widget>[
+  //           SimpleDialogOption(
+  //               padding: const EdgeInsets.all(20),
+  //               child: const Text('Take a photo'),
+  //               onPressed: () async {
+  //                 Navigator.pop(context);
+  //                 Uint8List file = await pickImage(ImageSource.camera);
+  //                 setState(() {
+  //                   _file = file;
+  //                 });
+  //               }),
+  //           SimpleDialogOption(
+  //               padding: const EdgeInsets.all(20),
+  //               child: const Text('Choose from Gallery'),
+  //               onPressed: () async {
+  //                 Navigator.of(context).pop();
+  //                 Uint8List file = await pickImage(ImageSource.gallery);
+  //                 setState(() {
+  //                   _file = file;
+  //                 });
+  //               }),
+  //           SimpleDialogOption(
+  //             padding: const EdgeInsets.all(20),
+  //             child: const Text("Cancel"),
+  //             onPressed: () {
+  //               Navigator.pop(context);
+  //             },
+  //           )
+  //         ],
+  //       );
+  //     },
+  //   );
+  // }
 
   void postImage(String uid, String username, String profImage) async {
     setState(() {
@@ -99,7 +101,8 @@ class _AddPostScreenState extends State<AddPostScreen> {
 
   void clearImage() {
     setState(() {
-      _file = null;
+      // TODO Go back to home
+      // _file = null;
     });
   }
 
@@ -113,89 +116,90 @@ class _AddPostScreenState extends State<AddPostScreen> {
   Widget build(BuildContext context) {
     final UserProvider userProvider = Provider.of<UserProvider>(context);
 
-    return _file == null
-        ? Center(
-            child: IconButton(
-              icon: const Icon(
-                Icons.upload,
-              ),
-              onPressed: () => _selectImage(context),
+    // return _file == null
+    //     ? Center(
+    //         child: IconButton(
+    //           icon: const Icon(
+    //             Icons.upload,
+    //           ),
+    //           onPressed: () => _selectImage(context),
+    //         ),
+    //       )
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: clearImage,
+        ),
+        title: const Text(
+          'Post to',
+        ),
+        centerTitle: false,
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => postImage(
+              userProvider.getUser.uid,
+              userProvider.getUser.username,
+              userProvider.getUser.photoUrl,
+            ),
+            child: const Text(
+              "Post",
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0),
             ),
           )
-        : Scaffold(
-            appBar: AppBar(
-              backgroundColor: mobileBackgroundColor,
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: clearImage,
-              ),
-              title: const Text(
-                'Post to',
-              ),
-              centerTitle: false,
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () => postImage(
-                    userProvider.getUser.uid,
-                    userProvider.getUser.username,
-                    userProvider.getUser.photoUrl,
-                  ),
-                  child: const Text(
-                    "Post",
-                    style: TextStyle(
-                        color: Colors.blueAccent,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0),
-                  ),
-                )
-              ],
-            ),
-            // POST FORM
-            body: Column(
-              children: <Widget>[
-                isLoading
-                    ? const LinearProgressIndicator()
-                    : const Padding(padding: EdgeInsets.only(top: 0.0)),
-                const Divider(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        userProvider.getUser.photoUrl,
-                      ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      child: TextField(
-                        controller: _descriptionController,
-                        decoration: const InputDecoration(
-                            hintText: "Write a caption...",
-                            border: InputBorder.none),
-                        maxLines: 8,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 45.0,
-                      width: 45.0,
-                      child: AspectRatio(
-                        aspectRatio: 487 / 451,
-                        child: Container(
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                            fit: BoxFit.fill,
-                            alignment: FractionalOffset.topCenter,
-                            image: MemoryImage(_file!),
-                          )),
-                        ),
-                      ),
-                    ),
-                  ],
+        ],
+      ),
+      // POST FORM
+      body: Column(
+        children: <Widget>[
+          isLoading
+              ? const LinearProgressIndicator()
+              : const Padding(padding: EdgeInsets.only(top: 0.0)),
+          const Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                  userProvider.getUser.photoUrl,
                 ),
-                const Divider(),
-              ],
-            ),
-          );
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.3,
+                child: TextField(
+                  controller: _descriptionController,
+                  decoration: const InputDecoration(
+                      hintText: "What\'s on your mind...",
+                      border: InputBorder.none),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                height: 45.0,
+                width: 45.0,
+                child: AspectRatio(
+                  aspectRatio: 487 / 451,
+                  child: Container(
+                      // TODO REDSIGN
+                      // decoration: BoxDecoration(
+                      //     image: DecorationImage(
+                      //   fit: BoxFit.fill,
+                      //   alignment: FractionalOffset.topCenter,
+                      //   image: MemoryImage(_file!),
+                      // )),
+                      ),
+                ),
+              ),
+            ],
+          ),
+          const Divider(),
+        ],
+      ),
+    );
   }
 }
