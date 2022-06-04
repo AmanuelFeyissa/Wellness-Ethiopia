e77c95e98363c4b5452b493e5bef5ba84de11e9f
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-06-04 10:04:17 +0300

    Update main.dart
    changed the name

--- wellness_ethiopia/lib/main.dart
+++ wellness_ethiopia/lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Instagram Clone',
+        title: 'Wellness Ethiopia',
         theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: mobileBackgroundColor,
         ),
