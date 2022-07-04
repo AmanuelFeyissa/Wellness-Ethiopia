7f65d539e31aa65d3aec36c91ab9f4eded3ce1c7
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-07-04 14:53:24 +0300

    added a new explore page

--- wellness_ethiopia/lib/screens/Explore_feature_screens/depression.dart
+++ wellness_ethiopia/lib/screens/Explore_feature_screens/depression.dart
@@ -12,44 +12,32 @@ class Depression extends StatelessWidget {
       // appBar: AppBar(
       //   title: Text('A Quick Description'),
       // ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    child: Text('Depression'),
-                    margin: const EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-                CommonCard(
-                  color: kPictureBackgroundColor,
-                  // Enter Image path here
-                  image: Image.asset('images/depression.png'),
-                ),
-              ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: CommonCard(
+                color: kPictureBackgroundColor,
+                // Enter Image path here
+                image: Image.asset('images/depression.png'),
+              ),
             ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-                child: Text(
-                  'Description \n \n $depressionDescription',
-                ),
-                margin: const EdgeInsets.all(15.0),
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: kPictureBackgroundColor,
-                )),
-          ),
-        ],
+            Expanded(
+              flex: 3,
+              child: Container(
+                  child: Text(
+                    'Description \n \n $depressionDescription',
+                  ),
+                  margin: const EdgeInsets.all(15.0),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: kPictureBackgroundColor,
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }
--- wellness_ethiopia/lib/screens/Explore_feature_screens/explore.dart
+++ wellness_ethiopia/lib/screens/Explore_feature_screens/explore.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wellness_ethiopia/screens/Explore_feature_screens/depression.dart';
+import 'package:wellness_ethiopia/widgets/commonCard.dart';
+import 'package:wellness_ethiopia/constants.dart';
 
 class Explore extends StatelessWidget {
   const Explore({Key? key}) : super(key: key);
@@ -9,6 +11,166 @@ class Explore extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Explore Page'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 5.0,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 15.0),
+            child: const Text(
+              'MENTAL ILLNESSES',
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(
+            height: 5.0,
+          ),
+          Row(
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Depression()));
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/depression.png'),
+                ),
+              ),
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // Route
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // Route
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // ROute
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // Route
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // Route
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 5.0,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 15.0),
+            child: const Text(
+              'MENTAL HEALTH INSTITUTIONS',
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(
+            height: 5.0,
+          ),
+          Row(
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // Route
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // Route
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // Route
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // ROute
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // Route
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+              Expanded(
+                child: CommonCard(
+                  onPressed: () {
+                    // Route
+                  },
+                  color: kPictureBackgroundColor,
+                  image: Image.asset('images/mentalPuzzle.png'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
